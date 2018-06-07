e6bcbe793c3196138b6f773b1c374aa3ce9a1d59
chenjiayao <690478206@qq.com>
2018-06-08 00:32:04 +0800

    xlsx，要支持2007版本才行

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import os
+import xlrd
+import xlwt
 
 def listFiles(dir):
     if not os.path.exists(dir):
@@ -17,3 +19,20 @@ def writeLines(file, lines):
     with open(file, 'w', encoding='utf8', buffering=2<<16) as f:
         for line in lines:
             f.write(line + "\n")
+def writeExcel(file, rows):
+    wb = xlwt.Workbook()
+    sheet = wb.add_sheet("source")
+    for i, row in enumerate(rows):
+        for j, cell in enumerate(row):
+            sheet.write(i, j, str(cell))
+    wb.save(file)
+def readExcel(file):
+    data = xlrd.open_workbook(file)
+    table = data.sheets()[0]
+    nrows = table.nrows #行数
+    for i in range(0, nrows):
+        yield table.row_values(i) #某一行数据
+
+rows = readExcel("D:/workspace/多多号.xlsx")
+rows = [row for row in rows]
+writeExcel("D:/workspace/out.xlsx", rows)
