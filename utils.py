97600e7298beb6cf4c59ed709024765c604d3fc4
chenjiayao <chenjiayao@aobi.com>
2018-06-08 18:19:33 +0800

    add 07excel

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
-import xlrd
-import xlwt
+import math
+import datetime
+import openpyxl
 
 def listFiles(dir):
     if not os.path.exists(dir):
@@ -15,24 +16,58 @@ def listFiles(dir):
             for sFile in listFiles(absFile):
                 yield sFile
     return 'done'
+def readLines(file):
+    with open(file, 'r', encoding='utf8', buffering=2<<16) as f:
+        while 1:
+            line=f.readline()
+            if line:
+                yield line
+            else:
+                return 'done'
 def writeLines(file, lines):
     with open(file, 'w', encoding='utf8', buffering=2<<16) as f:
         for line in lines:
             f.write(line + "\n")
-def writeExcel(file, rows):
-    wb = xlwt.Workbook()
-    sheet = wb.add_sheet("source")
+
+def benchmark(func):
+    def wrapper(*args, **kw):
+        b = datetime.datetime.now()
+        obj = func(*args, **kw)
+        e = datetime.datetime.now()
+        print('(%ss)call %s:' % ((e-b).seconds, func.__name__))
+        return obj
+    return wrapper
+
+def binsearch(list, e, key):
+    n = len(list)
+    bi,ei,mi = 0, n-1, 0
+    while 1:
+        mi = math.floor((bi+ei)/2)
+        if mi == bi or mi == ei:
+            return mi
+        if e == key(list[mi]):
+            return mi
+        elif e > key(list[mi]):
+            bi = mi
+        else:
+            ei = mi
+
+def writeExcel(path, rows):
+    wb = openpyxl.Workbook()
+    wb.guess_types = True
+    sheet = wb.active
     for i, row in enumerate(rows):
         for j, cell in enumerate(row):
-            sheet.write(i, j, str(cell))
-    wb.save(file)
-def readExcel(file):
-    data = xlrd.open_workbook(file)
-    table = data.sheets()[0]
-    nrows = table.nrows #行数
-    for i in range(0, nrows):
-        yield table.row_values(i) #某一行数据
+            sheet.cell(row=i+1, column=j+1, value=str(cell))
+    wb.save(path)
+def readExcel(path, sheet=None):
+    wb = openpyxl.load_workbook(path)
+    table = wb.active if sheet else wb.get_sheet_by_name(sheet)
+    for row in table.rows:
+        yield row
+    return row
 
-rows = readExcel("D:/workspace/多多号.xlsx")
-rows = [row for row in rows]
-writeExcel("D:/workspace/out.xlsx", rows)
+def test07Excel():
+    rows = [[1,2],[4,3],['A','b']]
+    write07Excel('E:/临时用文件夹/uc/test.xlsx', rows)
+#test07Excel()
